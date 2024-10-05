2fc006b2c9f68d13c7917dcf5459e42827286f6e
João <jps.pereira0805@gmail.com>
2024-10-05 10:14:38 -0300

    classe veículo: getters e setters

--- Veiculo.java
+++ Veiculo.java
@@ -1,3 +1,39 @@
 public abstract class Veiculo {
+    private String marca;
+    private String modelo;
+    private String placa;
+    private int ano;
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
     
 }
