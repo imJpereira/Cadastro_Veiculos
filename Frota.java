10b6b1a76fad5298f6e1ac34e3f9aea87d24fe25
João <jps.pereira0805@gmail.com>
2024-10-06 14:22:34 -0300

    frota: funções base

--- Frota.java
+++ Frota.java
@@ -1,3 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Frota {
-    
+    private List<Veiculo> listaDeVeiculos = new ArrayList<>();
+
+    public void adicionarLivro(Veiculo novoVeiculo) throws Exception {
+        
+        listaDeVeiculos.add(novoVeiculo);
+    }
+
+    public List<Veiculo> pesquisarPorTitulo(String placa) {
+        List<Veiculo> veiculosEncontrados = new ArrayList<>();
+        for (Veiculo veiculo : listaDeVeiculos) {
+            if (veiculo.getModelo().toLowerCase().contains(placa.toLowerCase())) {
+                veiculosEncontrados.add(veiculo);
+            }
+        }
+
+        return veiculosEncontrados;
+    }
+
+    public Boolean removerPorPlaca(String placa) {
+        for (Veiculo veiculo : listaDeVeiculos) {
+            if (veiculo.getPlaca().equalsIgnoreCase(placa)) {
+                listaDeVeiculos.remove(veiculo);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public List<Veiculo> pesquisarTodos() {
+        return this.listaDeVeiculos;
+    }
+
+    public List<Veiculo> getListaDVeiculos() {
+        return listaDeVeiculos;
+    }
 }
