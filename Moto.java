2fabffd9eea0c3eb9fd04409edea28af418cf2ac
Pedro <pedrorrien@gmail.com>
2024-10-07 19:41:50 -0300

    feat: carro moto caminhão

--- Moto.java
+++ Moto.java
@@ -1,3 +1,11 @@
 public class Moto extends Veiculo{
-    
+    private int cilindradas;
+
+    public int getCilindradas() {
+        return cilindradas;
+    }
+
+    public void setCilindradas(int cilindradas) {
+        this.cilindradas = cilindradas;
+    }
 }
